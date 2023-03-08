99e85727ea41a240156ab84d7b55662003e7251e
Milo C <75094742+whotf1@users.noreply.github.com>
2023-03-08 10:00:50 -0600

    added some more file and pages structuring

---
+++ portfolio/src/Components/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Header from "./Header"
+import Home from "./Home"
+
+
+
+ReactDOM.render(<Home/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
