50639e426772f08d45d9b21401573afe2c8a113c
Milo C <75094742+whotf1@users.noreply.github.com>
2023-03-12 23:21:01 -0500

    reseting repo to work from a template

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-
-
-export { default as ApiPage } from "./ApiPage"
-export { default as MapPage } from "./MapPage"
-export { default as SimpleForm } from "./Forms/SimpleForm"
-export { default as ComplexForm } from "./Forms/ComplexForm"
